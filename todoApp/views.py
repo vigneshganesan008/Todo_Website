d74038540a1cd4beb45819d49af525eb77bd9558
Vignesh Ganesan <vicky08rockz@gmail.com>
2021-01-14 19:05:06 +0530

    added login page and authentication

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,7 +1,9 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
 def signupUser(request):
@@ -9,7 +11,15 @@ def signupUser(request):
         return render(request,'todo/signup.html',{"form":UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            User.objects.create_user(request.POST['username'],request.POST['password1'])
-            return HttpResponse("created")
+            try:
+                user = User.objects.create_user(request.POST['username'],request.POST['password1'])
+                user.save()
+                login(request,user)
+                return redirect('currentTodos')
+            except IntegrityError:
+                return render(request,'todo/signup.html',{"form":UserCreationForm(),"error":"Username already exists"})
         else:
-            return HttpResponse("password does not match")
+            return render(request,'todo/signup.html',{"form":UserCreationForm(),"error":"Passwords does not match"})
+
+def currentTodos(request):
+    return render(request,'todo/currentTodos.html')
