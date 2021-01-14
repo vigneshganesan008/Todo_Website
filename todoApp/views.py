3ae793f8442875e86cfa300e05c3f9fb438c6475
Vignesh Ganesan <vicky08rockz@gmail.com>
2021-01-14 18:32:13 +0530

    first commit

---
+++ todoApp/views.py
@@ -0,0 +1,15 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# Create your views here.
+def signupUser(request):
+    if request.method == "GET":
+        return render(request,'todo/signup.html',{"form":UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            User.objects.create_user(request.POST['username'],request.POST['password1'])
+            return HttpResponse("created")
+        else:
+            return HttpResponse("password does not match")
